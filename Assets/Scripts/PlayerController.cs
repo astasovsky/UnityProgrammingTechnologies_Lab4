0e9e2b88afe0d0438e2e4fbccad55ef6a1fb6417
astasovsky <stasovsky.ua@gmail.com>
2022-10-27 21:48:37 +0300

    Task 4.1 Watch Where You’re Going. Done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform focalPoint;
+
+    private const float Speed = 5;
+    private Rigidbody _playerRigidbody;
+
+    private void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        _playerRigidbody.AddForce(Speed * forwardInput * focalPoint.transform.forward);
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    private const float RotationSpeed = 50;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * RotationSpeed * Time.deltaTime);
+    }
+}
